359ba068b44f1691fb12d920200b2f6b0a2cea94
KarmaZA <EVRJON003@myuct.ac.za>
2020-05-19 13:33:40 +0200

    Added menu scene
    Added a Base outline of the menu scene and got the code for button click to work

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quit : MonoBehaviour
+{
+    public Button button;
+    private void Start()
+    {
+        button.onClick.AddListener(QuitGame);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("test");
+        Application.Quit(); //Test this with fully built app
+    }
+}
